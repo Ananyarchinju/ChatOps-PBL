b1e72bdda20e923926777a8c3b3227eea353bb4b
Ananyarchinju <Ananyarchinju@users.noreply.github.com>
2026-06-03 01:22:54 +0530

    Remove test-trigger.ts to fix Jenkins TS build error

--- backend/test-trigger.ts
+++
@@ -1,4 +0,0 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-import { jenkinsService } from './src/services/JenkinsService';
-jenkinsService.triggerBuild('ChatOps').then(console.log).catch(console.error);
